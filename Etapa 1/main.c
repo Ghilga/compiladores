3cdc411c2e73c86dd9c941b126c5bc2e96ce7e52
Leonardo <leosaugusto98@gmail.com>
2022-01-26 00:57:11 -0300

    etapa 1 update

--- Etapa 1/main.c
+++ Etapa 1/main.c
@@ -1,12 +1,43 @@
 //Autor: Leonardo de Souza Augusto - 278998
 
 #include <stdio.h>
+#include "tokens.h"
+#include "hash.h"
 
 extern int running;
+extern int isRunning();
+extern int yylex();
+extern FILE* yyin;
+FILE *inputFile;
 
-int main (){
-    printf("Lex Started!\n");
+void initMe(char* filename){
     running = 1;
-    yylex();
-    running = 0;
+
+    hashInit();
+
+    if ( (inputFile = fopen(filename, "r")) == NULL ) {
+        printf("Error opening file\n");
+        exit(1);
+    }
+
 }
+
+int main (int argc, char** argv) {
+    if (argc < 2){
+       printf("Call program with 'filename.txt'\n");
+       exit(1);
+    }
+
+    initMe(argv[1]);
+    yyin = inputFile;
+
+    while(isRunning()){
+        int token = yylex();
+        if (token == 0){
+            printf("Error %d\n", TOKEN_ERROR);
+        }
+        printf("Token retornado: %d, %c\n", token, token);
+    }
+    exit(0);
+}
+
