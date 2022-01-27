15b3989a7495cd8c902e717061df0f8a447732fb
Leonardo <leosaugusto98@gmail.com>
2022-01-26 22:43:41 -0300

    versao final etapa1

--- Etapa 1/main.c
+++ Etapa 1/main.c
@@ -1,43 +1,41 @@
 //Autor: Leonardo de Souza Augusto - 278998
 
 #include <stdio.h>
-#include "tokens.h"
-#include "hash.h"
 
-extern int running;
+
 extern int isRunning();
+//sdasd
 extern int yylex();
-extern FILE* yyin;
+extern char *yytext;
+extern FILE *yyin;
 FILE *inputFile;
 
-void initMe(char* filename){
-    running = 1;
+void initMe(char *filename) {
+    //hashInit();
 
-    hashInit();
-
-    if ( (inputFile = fopen(filename, "r")) == NULL ) {
+    if ((inputFile = fopen(filename, "r")) == NULL) {
         printf("Error opening file\n");
         exit(1);
     }
-
 }
 
-int main (int argc, char** argv) {
-    if (argc < 2){
-       printf("Call program with 'filename.txt'\n");
-       exit(1);
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Call program with 'filename.txt'\n");
+        exit(1);
     }
 
     initMe(argv[1]);
     yyin = inputFile;
+    int token = -1;
 
-    while(isRunning()){
-        int token = yylex();
-        if (token == 0){
-            printf("Error %d\n", TOKEN_ERROR);
+    while (isRunning()) {
+        token = yylex();
+
+        if (!isRunning()) {
+            break;
         }
-        printf("Token retornado: %d, %c\n", token, token);
+        printf("Texto: %s | Token: %d\n", yytext, token);
     }
     exit(0);
 }
-
